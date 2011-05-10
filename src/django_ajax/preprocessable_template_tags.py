75bf4c8e5b866780b4a51fe61aa5fcbf26a6a674
Jonathan Slenders <jonathan.slenders@city-live.be>
2011-05-10 10:23:16 +0200

    added preprocessable template tags (allow preprocessing of the tabbing template tags in the templates.

---
+++ src/django_ajax/preprocessable_template_tags.py
@@ -0,0 +1,40 @@
+
+
+# Preprocess following tags in the template preprocessor.
+# https://github.com/citylive/django-template-preprocessor
+
+
+
+from template_preprocessor import preproces_tag
+
+
+
+@preprocess_tag
+def tabpage(*args):
+    return u'<div class="tabbing">'
+
+@preprocess_tag
+def tabs(*args):
+    return u'<div class="tabbing-tabs">'
+
+@preprocess_tag
+def endtabs(*args):
+    return u'</div>'
+
+@preprocess_tag
+def tabcontent(*args):
+    if hasattr(settings, 'STATIC_URL'):
+        loader = '<img src="%sajax-utilities/img/loader.gif" alt="%s" />' % (settings.STATIC_URL, _('Loading'))
+    else:
+        loader = '<img src="%scommon/img/loader.gif" alt="%s" />' % (settings.MEDIA_URL, _('Loading'))
+
+    return (u'<div class="tabbing-loader" style="display:none;">%s %s</div>'
+                u'<div class="tabbing-content">') % (loader, _('Loading data...'))
+
+@preprocess_tag
+def endtabcontent(*args):
+    return u'</div>'
+
+@preprocess_tag
+def endtabpage(*args):
+    return u'</div>'
