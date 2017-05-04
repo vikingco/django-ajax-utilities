c595eff3110453f3df74596ee4686102814a7157
Jeroen Op 't Eynde <jeroen@vikingco.com>
2017-05-04 16:37:00 +0200

    Prep for pushing to Pypi

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-ajax-utilities',
-    version='2.0.0',
+    version='1.2.7',
     url='https://github.com/vikingco/django-ajax-utilities',
     license='BSD',
     description='Pagination, xhr and tabbing utilities for the Django framework.',
     long_description=open('README', 'r').read(),
-    author='Unleashed nv',
-    packages=find_packages('src'),
+    author='Unleashed NV',
+    author_email='operations@unleashed.be',
+    packages=find_packages('.'),
     package_data={'django_ajax': [
                 'static/*.js', 'static/*/*.js', 'static/*/*/*.js',
                 'static/*.css', 'static/*/*.css', 'static/*/*/*.css',
@@ -16,9 +17,8 @@ setup(
                 'static/*.gif', 'static/*/*.gif', 'static/*/*/*.gif', 'static/*/*/*/*.gif',
                 'templates/*.html', 'templates/*/*.html', 'templates/*/*/*.html'
                 ]},
-    zip_safe=False,  # Don't create egg files, Django cannot find templates in egg files.
+    zip_safe=False,
     include_package_data=True,
-    package_dir={'': 'src'},
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python',
